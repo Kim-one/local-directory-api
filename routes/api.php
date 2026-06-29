9d14cd20eddbe8522d94891b09a3cafd06a1f43d
kim-one <kimonebarrett16@gmail.com>
2026-06-29 08:24:12 -0300

    Move auth routes to api.php, remove CSRF requirement

--- routes/api.php
+++ routes/api.php
@@ -1,47 +1,34 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\api\BusinessController;
 use App\Http\Controllers\api\ReviewController;
 
+// Public routes
+Route::post('/login', Login::class);
 Route::post('/register', Register::class);
 
 Route::get('/businesses', [BusinessController::class, 'index']);
-
 Route::get('/businesses/{slug}', [BusinessController::class, 'show']);
+Route::get('/businesses/{slug}/reviews', [ReviewController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    $user = $request->user();
-    return response()->json([
-        'name'  => $user->firstName . ' ' . $user->lastName,
-        'email' => $user->email,
-    ]);
-});
-
+// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        $user = $request->user();
+        return response()->json([
+            'name'  => $user->firstName . ' ' . $user->lastName,
+            'email' => $user->email,
+        ]);
+    });
+    Route::post('/logout', Logout::class);
     Route::get('/my-businesses', [BusinessController::class, 'myBusinesses']);
     Route::post('/businesses', [BusinessController::class, 'store']);
-    Route::get('/businesses/{slug}', [BusinessController::class, 'show']);
     Route::post('/businesses/{slug}', [BusinessController::class, 'update']);
     Route::delete('/businesses/image/{imageId}', [BusinessController::class, 'deleteImage']);
-    Route::get('/businesses/{slug}/reviews',          [ReviewController::class, 'index']);
-    Route::post('/businesses/{slug}/reviews',         [ReviewController::class, 'store']);
-    Route::delete('/businesses/{slug}/reviews/{reviewId}',  [ReviewController::class, 'destroy']);
+    Route::post('/businesses/{slug}/reviews', [ReviewController::class, 'store']);
+    Route::delete('/businesses/{slug}/reviews/{reviewId}', [ReviewController::class, 'destroy']);
 });
-
-// Route::get('/view-all', function() {
-//     return App\Models\User::all();
-// });
-//
-// Route::get('/all-businesses', function() {
-//     return App\Models\Business::all();
-// });
-//
-// Route::post('/register', Register::class);
-// Route::post('/createBusiness', BusinessController::class);
-//
-// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-//     return $request->user();
-// });
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\Login;
-use App\Http\Controllers\Auth\Logout;
-
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::middleware('web')->group(function () {
-    Route::post('/login', Login::class);
-    Route::post('/logout', Logout::class);
 });
