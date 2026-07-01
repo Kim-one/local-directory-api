d48fc6fcaf2af237b3a75d7f49657c4e663640bd
kim-one <kimonebarrett16@gmail.com>
2026-07-01 13:05:03 -0300

    updated BusinessImage model

--- app/Models/BusinessImage.php
+++ app/Models/BusinessImage.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Storage;
 
 class BusinessImage extends Model
 {
     protected $fillable = ['business_id', 'path', 'type'];
+
+    protected function url(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => Storage::disk('s3')->url($this->path),
+        );
+    }
 }
